5b7ba4a2c937e31be5ff5d64b7556bd7e7020ef4
Luigi Consentino <luigi.aconsentino@gmail.com>
2023-10-18 17:00:27 -0300

    Initial commit

---
+++ src/test/java/javarestapi/luigi/JavaRestApiProjectSantanderBootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package javarestapi.luigi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiProjectSantanderBootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
